54fa0c21ecd7644714c8cecc6d7454581829451c
niklas@astne.dk <niklas@astne.dk>
2023-07-26 20:43:18 +0200

    added tailwind

---
+++ wp_dev_local/wordpress/wp-content/themes/n5_demo/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.php, *./src/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+}
+
